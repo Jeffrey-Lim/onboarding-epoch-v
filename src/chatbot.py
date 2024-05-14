3aa01b9ffb7143da9b3d8397ce48a339c99b5020
madhavv197 <madhavv197@gmail.com>
2024-05-14 20:45:16 +0200

    Add random name option

--- src/chatbot.py
+++ src/chatbot.py
@@ -3,6 +3,8 @@
 from collections.abc import Callable
 from dataclasses import dataclass
 
+import randomname
+
 
 @dataclass(slots=True)
 class ChatBot:
@@ -28,10 +30,15 @@ class ChatBot:
 
     # TODO(Jeffrey): Add option "Give me a random name"
 
+    def get_random_name(self) -> str:
+        """Run randomname method get name.
+
+        :return: random name string
+        """
+        return randomname.get_name()
+
     # TODO(Jeffrey): Add option "Give the answer to life, the universe, and everything"
 
     def get_conversation_options(self) -> list[tuple[str, Callable[[], str]]]:
         """TODO(Jeffrey): Document this method."""
-        return [
-            ("Say hello", self.say_hi),
-        ]
+        return [("Say hello", self.say_hi), ("Give me a random name", self.get_random_name)]
